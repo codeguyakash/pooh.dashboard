6c685350224f32b5acd84fafe8474bf3380477ff
Akash (codeguyakash) <codeguyakash@gmail.com>
2025-08-11 11:58:58 +0530

    Update margin in Home component header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import React, { Suspense } from 'react';
 export default function Home() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-   
-      <h1 className="text-center mt-[28]">Hello</h1>
+      <h1 className="text-center mt-[20px]">Hello</h1>
     </Suspense>
   );
 }
