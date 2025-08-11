6b3d295b1a151f32e1c8cd1d089c5d5ff50f7ed1
Akash (codeguyakash) <codeguyakash@gmail.com>
2025-08-11 11:58:18 +0530

    FF

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 import React, { Suspense } from 'react';
-// import ResetPasswordPage from './reset-password/page';
-// import { useSearchParams } from 'next/navigation';
 
 export default function Home() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      {/* <ResetPasswordPage /> */}
-      <h1>Hello</h1>
+   
+      <h1 className="text-center mt-[28]">Hello</h1>
     </Suspense>
   );
 }
